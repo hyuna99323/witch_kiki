557487cba5460b1a7f920df932f0124409d2e23e
hyunao <89890370+hyuna99323@users.noreply.github.com>
2024-02-14 00:55:13 +0900

    반응형을 위한 파일 분배

--- js/burgermenu.js
+++ js/burgermenu.js
@@ -1,25 +1,3 @@
-var darkBackground = document.querySelector(".dark-background");
-var navbar = document.querySelector(".navbar_main");
-
-function handleNavbarScroll1() {
-  var windowHeight = window.innerHeight;
-  var scrollPosition = window.scrollY;
-
-  var darkBackgroundHeight = darkBackground.offsetHeight;
-  var darkBackgroundTop = darkBackground.offsetTop;
-
-  var triggerPosition1 = darkBackgroundTop + (darkBackgroundHeight * 0.9);
-
-  if (scrollPosition >= triggerPosition1) {
-    navbar.classList.add("navbar_scroll");
-  } else {
-    navbar.classList.remove("navbar_scroll");
-  }
-}
-
-window.addEventListener("scroll", handleNavbarScroll1);
-window.addEventListener("resize", handleNavbarScroll1);
-
 const navbarBurger = document.querySelector('.gnb');
 const navbarburgerback = document.querySelector('.hurger_back');
 const navbarMenu = document.querySelector('.navbar_menu');
