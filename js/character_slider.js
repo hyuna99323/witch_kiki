2ee0e06ddcbcd656a71076e5fb99c5c18cb17b5c
hyunao <89890370+hyuna99323@users.noreply.github.com>
2024-02-19 17:34:56 +0900

    반응형 완성

--- js/character_slider.js
+++ js/character_slider.js
@@ -1,7 +1,8 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
+  // slidesPerView: 1.8,
+  // slidesperGroup: 1,
   centeredSlides: true,
-  spaceBetween: 30,
+  spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
     type: "fraction",
@@ -12,4 +13,29 @@ var swiper = new Swiper(".mySwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  breakpoints: {
+    320 : {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    768 : {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    1280 : {
+      slidesPerView: 1.05,
+      slidesperGroup: 2,
+      spaceBetween: 20,
+    },
+    1440 : {
+      slidesPerView: 1.2,
+      slidesperGroup: 1,
+      spaceBetween: 20,
+    },
+    1920 : {
+      slidesPerView: 1.2,
+      slidesperGroup: 3,
+      spaceBetween: 40,
+    }
+  }
 });
